8f0a3c189de4f62bfcbf6985f27bce3e79958c05
MoNigma <mohamed.5509@live.nl>
2020-06-29 00:37:58 +0200

    fixed shipplacement bugs

--- public/javascripts/ShipPlacement.js
+++ public/javascripts/ShipPlacement.js
@@ -23,39 +23,44 @@ var arr = [
                     
         x = $(e.target).data('x');
         y = $(e.target).data('y');
-        if(arr[x][y] == 1){
-            alert("You already placed a ship here! Try another tile.");
-            return;
+
+        if (arr[y][x] == 1) {
+            e.target.removeAttribute("style");
+            arr[y][x] = 0;
+            placeCounter--;
         }
-        else{
-                arr[x][y] = 1;
-                e.target.style.background = 'green';
-                console.log("Player:  has clicked on coordinates (" + x + "," + y + ").");
-                placeCounter++;
-                switch(placeCounter){
-                            case 17:
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Click start!</span>");
-                                $('#startgamebutton').replaceWith("<button id=\"startgamebutton\" value=\"START\">Start Game</button>");
-                                break;
-                            case 15:
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place now a ship of 2 Tiles</span>");
-                                break;
-                            case 12:
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place now another ship of 3 Tiles</span>");
-                                break;
-                            case 9:
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place now a ship of 3 Tiles</span>");
-                                break;
-                            case 5:
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place now aship of 3 Tiles</span>");
-                                break;
-                            case 0: 
-                                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place now a ship of 5 Tiles</span>");
-                                break;
-                            }; 
-                }
-                console.log("PLACECOUNTER: " + placeCounter);
-            });
-        
+        else {
+            arr[y][x] = 1;
+            e.target.style.background = 'green';
+            console.log("Player:  has clicked on coordinates (" + y + "," + x + ").");
+            placeCounter++;
+        }
+
+        switch (placeCounter) {
+            case 17:
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Click start!</span>");
+                $('#startgamebutton').replaceWith("<button id=\"startgamebutton\" type=\"submit\">Start Game</button>");
+                break;
+            case 15:
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place the <b>Patrol Boat</b> on the grid</span>");
+                break;
+            case 12:
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place the <b>Submarine</b> on the grid</span>");
+                break;
+            case 9:
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place the <b>Destroyer</b> ship on the grid</span>");
+                break;
+            case 5:
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place the <b>Battleship</b> on the grid</span>");
+                break;
+            case 0: 
+                $('#notification').replaceWith("<span class=\"marquee\" id=\"notification\">Place the <b>Aircraft Carrier</b> on the grid</span>");
+                break;
+        }; 
+
+        console.log("PLACECOUNTER: " + placeCounter);
+        console.log(arr)
+
+    });
 
 });

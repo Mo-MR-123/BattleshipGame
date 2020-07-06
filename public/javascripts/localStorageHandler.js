69b295ca8fd4c3768f6f24716174107471359bdb
MoNigma <mohamed.5509@live.nl>
2020-07-06 21:25:14 +0200

    created localStorage handler file

---
+++ public/javascripts/localStorageHandler.js
@@ -0,0 +1,42 @@
+/* Helper functions to handle everything needed to be handled in localStorage */
+
+(function (exports) {
+
+    // check if localStorage is supported on the user browser
+    if (typeof(Storage) !== "undefined") {
+
+        // Adding object (e.g. { name: "Obaseki Nosa", location: "Lagos" } ) to localStorage
+        exports.addObject = function(objectName, objectValue) {
+            if (!(objectName instanceof String)) {
+                return new Error(`${objectName} is not of a String type. Make sure the object name is a String.`);
+            }
+            window.localStorage.setItem(objectName, JSON.stringify(objectValue));
+        };
+
+        // Getting an object from localStorage
+        exports.getObject = function(objectName) {
+            if (!(objectName instanceof String)) {
+                return new Error(`${objectName} is not of a String type. Make sure the object name is a String.`);
+            }
+            JSON.parse(window.localStorage.getItem(objectName));
+        };
+
+        // Remove object from localStorage
+        exports.removeObject = function(objectName) {
+            if (!(objectName instanceof String)) {
+                return new Error(`${objectName} is not of a String type. Make sure the object name is a String.`);
+            }
+            window.localStorage.removeItem(objectName);
+        };
+
+        // Clear the whole localStorage (deletes everything present in localstorage)
+        exports.clearLocalStorage = function() {
+            window.localStorage.clear();
+        };
+
+    } else {
+        return new Error("No localStorage available in this browser. Please make sure you use a browser that supports localStorage!");
+    }
+} 
+(typeof exports === "undefined" ? this.localStorage = {} : new Error("Don't use this in server please.")));
+

a93672aafa72829b77a24d92481bde24b28c0ce7
MoNigma <mohamed.5509@live.nl>
2020-07-07 02:11:54 +0200

    enabled sameSite of each session cookie to prevent warnings that have to do with sameSite policy of browsers

--- sessionConfig.js
+++ sessionConfig.js
@@ -7,9 +7,10 @@ const sessionConfig = {
     // "saveUninitialized" keeps and stores the session id in-memory even though the session is empty
     // NOTE: it can also be saved in REDIS in-memory database for fast and efficient session ids retrieval/store
     saveUninitialized: false,
-    // cookie : {
-    //   sameSite: 'strict',
-    // }
+    // every session cookie must have sameSite equals to "strict" to adhere to security changes of browsers
+    cookie : {
+      sameSite: 'strict',
+    }
 }
 
 module.exports = sessionConfig;
